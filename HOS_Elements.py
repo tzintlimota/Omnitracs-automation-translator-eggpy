700653b7e897379f22b34c49af11c834e97e9afb
EderAOrtega <eortega@omnitracs.com>
2021-03-03 17:04:53 -0600

    Adding changes

--- HOS_Elements.py
+++ HOS_Elements.py
@@ -4,8 +4,9 @@ from selenium.webdriver.common.by import By
 class webElem():
     #----- Administration page -----
     ADMIN_URL = "https://hos.int.omnitracs.com/QHOS/~admin/admin.aspx"
-    ADMIN_DRIVERS_LINK = (By.ID,"AdminUC1_hlDrivers")
+    ADMIN_DRIVER_LINK = (By.ID,"AdminUC1_hlDrivers")
     ADMIN_CARRIER_LINK = (By.ID,"AdminUC1_h1Carriers")
+    ADMIN_VEHICLE_LINK = (By.ID,"AdminUC1_hlVehicles")
 
     #----- Driver administration page -----
     DA_SEARCH_DEPOT_TXT = (By.ID,"MDriverUC1_searchDepotTextBox")
@@ -15,4 +16,7 @@ class webElem():
     #----- Carrier administration page -----
     CA_CARRIER_SELECT = (By.ID,"MCarrierUC1_ddlCarriers")
 
-
+    #----- Vehicle administration page -----
+    VA_SEARCH_DEPOT_TXT = (By.ID,"MVehicleUC1_searchDepotTextBox")
+    VA_SEARCH_DEPOT_ALLVEHI_OPT = (By.LINK_TEXT,"--- All vehicles ---")
+    VA_SEARCH_VEHICLE_TXT = (By.ID,"MVehicleUC1_searchVehicleTextBox")
--- WinMachine_Common.py
+++ WinMachine_Common.py
@@ -18,6 +18,19 @@ class WinMachine_Common:
 
         globalDriver.find_element(webElem.CA_CARRIER_SELECT).send_keys(carrier,Keys.ENTER)
 
+    def goToVehicleInAdmin(UA):
+        globalDriver.get(webElem.ADMIN_URL)
+        globalDriver.find_element(webElem.ADMIN_VEHICLE_LINK).click()
+
+        globalDriver.find_element(webElem.VA_SEARCH_DEPOT_TXT).clear()
+        globalDriver.find_element(webElem.VA_SEARCH_DEPOT_TXT).send_keys("All vehicles")
+        globalDriver.find_element(webElem.VA_SEARCH_DEPOT_ALLVEHI_OPT).click()
+
+        globalDriver.find_element(webElem.VA_SEARCH_VEHICLE_TXT).clear()
+        globalDriver.find_element(webElem.VA_SEARCH_VEHICLE_TXT).send_keys(UA)
+
+        globalDriver.find_element_by_partial_link_text(UA).click()
+
 
 
 
