0b69c0fc521038a89dd9e37f7973420294cb8bce
EderAOrtega <eortega@omnitracs.com>
2021-03-05 17:06:53 -0600

    Uploading changes
    Added new selenium functions.

--- HOS_Elements.py
+++ HOS_Elements.py
@@ -4,14 +4,22 @@ from selenium.webdriver.common.by import By
 class webElem():
     #----- Administration page -----
     ADMIN_URL = "https://hos.int.omnitracs.com/QHOS/~admin/admin.aspx"
+    ADMIN_DRIVER_URL = "https://hos.int.omnitracs.com/QHOS/~admin/mdriver.aspx"
     ADMIN_DRIVER_LINK = (By.ID,"AdminUC1_hlDrivers")
     ADMIN_CARRIER_LINK = (By.ID,"AdminUC1_h1Carriers")
     ADMIN_VEHICLE_LINK = (By.ID,"AdminUC1_hlVehicles")
+    ADMIN_HOSSETUP_LINK = (By.ID,"AdminUC1_hlHOSSetup")
 
     #----- Driver administration page -----
     DA_SEARCH_DEPOT_TXT = (By.ID,"MDriverUC1_searchDepotTextBox")
     DA_SEARCH_DEPOT_ALLDRI_OPT = (By.LINK_TEXT,"--- All drivers ---")
     DA_SEARCH_DRIVER_TXT = (By.ID,"MDriverUC1_searchDriverTextBox")
+    DA_EXEMPT_RADIO_YES = (By.ID,"MDriverUC1_rblELDExemption_0")
+    DA_REASON_TXT = (By.ID,"MDriverUC1_txtELDComment")
+    DA_EXEMPT_RADIO_NO = (By.ID,"MDriverUC1_rblELDExemption_1")
+    DA_SAVE_BUTTON = (By.ID,"MDriverUC1_cmdSave")
+    DA_YARDMOVE_RADIO_YES = (By.ID,"MDriverUC1_rblYardMove_0")
+    DA_YARDMOVE_RADIO_NO = (By.ID,"MDriverUC1_rblYardMove_1")
 
     #----- Carrier administration page -----
     CA_CARRIER_SELECT = (By.ID,"MCarrierUC1_ddlCarriers")
@@ -20,3 +28,30 @@ class webElem():
     VA_SEARCH_DEPOT_TXT = (By.ID,"MVehicleUC1_searchDepotTextBox")
     VA_SEARCH_DEPOT_ALLVEHI_OPT = (By.LINK_TEXT,"--- All vehicles ---")
     VA_SEARCH_VEHICLE_TXT = (By.ID,"MVehicleUC1_searchVehicleTextBox")
+
+    #----- Driver details page -----
+    DD_DUTYSTATUSCHANGES_SELECT = (By.ID,"ddlTimeFrame")
+    DD_24HRS_SELECT_OPT = (By.XPATH,"//select[@id='ddlTimeFrame']/option[text()='Past 24 hours']")
+    DD_DRIVINGSWAP_BUTTON = (By.XPATH,"//table[@id='dgDetails']//input[contains(@id,'dgDetails_cmdDrvTimeSwap_')]")
+
+    #----- Driver's configuration popup -----
+    DCP_SPLIT_ICON = (By.ID,"imgSplit")
+    DCP_DUTY1_SELECT = (By.ID,"ddlDuty1")
+    DCP_EXCEPTION1_SELECT = (By.ID,"ddlEventFlag1")
+    DCP_DUTY2_SELECT = (By.ID, "ddlDuty2")
+    DCP_EXCEPTION2_SELECT = (By.ID, "ddlEventFlag2")
+    DCP_EDITREASON_TXT = (By.ID,"comboboxEditReason")
+    DCP_SAVE_BUTTON = (By.ID,"cmdChange")
+
+    #----- Driving swap popup -----
+    DSP_DRIVERID_TEXT = (By.ID,"DrvIDTextBox")
+    DSP_SAVE_BUTTON = (By.ID,"cmdSwap")
+    DSP_SWAPSUCCESS_MSGBAR = (By.ID,"messageBar_imgMessage")
+
+    #----- HOS setup page -----
+    HS_YARDMOVE_CHECKBOX = (By.ID,"rblYardMoveEnable_1")
+    HS_POWER_CHECKBOX = (By.ID,"rblYardMoveTermination_0")
+    HS_SPEED_CHECKBOX = (By.ID, "rblYardMoveTermination_1")
+    HS_BOTH_CHECKBOX = (By.ID, "rblYardMoveTermination_2")
+    HS_SAVE_BUTTON = (By.ID,"cmdSave")
+
--- WinMachine_Common.py
+++ WinMachine_Common.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.common.action_chains import ActionChains
 from HOS_Elements import webElem
 
 #Global variable to store web driver
 globalDriver = webdriver.Chrome()
+globalDriver.implicitly_wait(10)
 
 class WinMachine_Common:
     def some_method(self):
@@ -31,12 +33,200 @@ class WinMachine_Common:
 
         globalDriver.find_element_by_partial_link_text(UA).click()
 
+    def editCertStatus(preStatus,type,change,recordNum,time,exceptionStatus):
+        globalDriver.find_element(webElem.DD_DUTYSTATUSCHANGES_SELECT).click()
+        if time != None:
+            timeOption = globalDriver.find_element_by_xpath("//select[@id='ddlTimeFrame']/option[text()='"+time+"']")
+            timeOption.click()
+        else:
+            globalDriver.find_element(webElem.DD_24HRS_SELECT_OPT).click()
 
+        preStatusesList = globalDriver.find_elements_by_xpath("//table[@id='dgDetails']//img[contains(@id,'dgDetails_imgConfirmed_')]/following-sibling::span[text()='"+preStatus+"']/..//input[contains(@id,'dgDetails_cmdCorrect_')]")
+        if preStatusesList.count() == 0:
+            preStatusesList = globalDriver.find_elements_by_xpath("//table[@id='dgDetails']//img[contains(@id,'dgDetails_imgConfirmed_')]/following-sibling::span[text()='"+preStatus+"']/../..//input[contains(@id,'dgDetails_cmdCorrect_')]")
 
+        assert preStatusesList.count() != 0
 
+        firstButton = preStatusesList[0]
 
+        locatorID = firstButton.get_property('id')
+        splitLocator = locatorID.split("_")
+        finalID = splitLocator[-1]
 
+        firstButton.click()
 
+        currentWindow = globalDriver.current_window_handle
+        openWindows = globalDriver.window_handles
+        globalDriver.switch_to.window(openWindows[-1])
+
+        if type == "Split":
+            globalDriver.find_element(webElem.DCP_SPLIT_ICON).click()
+            if recordNum == 1:
+                globalDriver.find_element(webElem.DCP_DUTY1_SELECT).click()
+                globalDriver.find_element_by_xpath("//select[@id='ddlDuty1']/option[contains(text(),'"+change+"')]").click()
+                if not exceptionStatus == None:
+                    globalDriver.find_element(webElem.DCP_EXCEPTION1_SELECT).click()
+                    globalDriver.find_element_by_xpath("//select[@id='ddlEventFlag1']/option[contains(text(),'"+exceptionStatus+"')]").click()
+
+            elif recordNum == 2:
+                globalDriver.find_element(webElem.DCP_DUTY1_SELECT).click()
+                globalDriver.find_element_by_xpath("//select[@id='ddlDuty2']/option[contains(text(),'" + change + "')]").click()
+                if not exceptionStatus == None:
+                    globalDriver.find_element(webElem.DCP_EXCEPTION2_SELECT).click()
+                    globalDriver.find_element_by_xpath("//select[@id='ddlEventFlag2']/option[contains(text(),'"+exceptionStatus+"')]").click()
+
+        elif type == "Edit":
+            globalDriver.find_element(webElem.DCP_DUTY1_SELECT).click()
+            globalDriver.find_element_by_xpath("//select[@id='ddlDuty1']/option[contains(text(),'" + change + "')]").click()
+            if not exceptionStatus == None:
+                globalDriver.find_element(webElem.DCP_EXCEPTION1_SELECT).click()
+                globalDriver.find_element_by_xpath("//select[@id='ddlEventFlag1']/option[contains(text(),'" + exceptionStatus + "')]").click()
+
+        globalDriver.find_element(webElem.DCP_EDITREASON_TXT).click()
+        globalDriver.find_element(webElem.DCP_EDITREASON_TXT).send_keys("Testing")
+
+        globalDriver.find_element(webElem.DCP_SAVE_BUTTON).click()
+
+        globalDriver.switch_to.window(currentWindow)
+
+        editedID = "dgDetails_lblActivity_"+finalID
+
+        try:
+            editedRecord = globalDriver.find_element_by_xpath("//span[@id='"+editedID+"' and text()='"+change+" - Pending']")
+        except NoSuchElementException:
+            raise RuntimeError("Unable to find the edited record on the table.")
+
+    def swapDrivingStatus (time, driverId, newStatus, reason, index):
+        globalDriver.find_element(webElem.DD_DUTYSTATUSCHANGES_SELECT).click()
+        if time != None:
+            timeOption = globalDriver.find_element_by_xpath("//select[@id='ddlTimeFrame']/option[text()='" + time + "']")
+            timeOption.click()
+        else:
+            globalDriver.find_element(webElem.DD_24HRS_SELECT_OPT).click()
+
+        driveSwapButtonList = globalDriver.find_elements(webElem.DD_DRIVINGSWAP_BUTTON)
+        swapButton = driveSwapButtonList[index]
+        locatorID = swapButton.get_property("id")
+        locatorID.split("_")
+        idNumber = locatorID[-1]
+        swapButton.click()
+
+        currentWindow = globalDriver.current_window_handle
+        openWindows = globalDriver.window_handles
+        globalDriver.switch_to.window(openWindows[-1])
+
+        globalDriver.find_element(webElem.DSP_DRIVERID_TEXT).clear()
+        globalDriver.find_element(webElem.DSP_DRIVERID_TEXT).send_keys(driverId)
+
+        if newStatus != None:
+            globalDriver.find_element(webElem.DCP_DUTY2_SELECT).click()
+            globalDriver.find_element_by_xpath("//select[@id='ddlDuty2']/option[text()='"+newStatus+"']")
+
+        globalDriver.find_element(webElem.DCP_EDITREASON_TXT).click()
+
+        if reason != None:
+            globalDriver.find_element(webElem.DCP_EDITREASON_TXT).send_keys(reason)
+        else:
+            globalDriver.find_element(webElem.DCP_EDITREASON_TXT).send_keys("TESTING")
+
+        globalDriver.find_element(webElem.DSP_SAVE_BUTTON).click()
+
+        try:
+            failElement =  globalDriver.find_element(webElem.DSP_SWAPSUCCESS_MSGBAR)
+            raise RuntimeError("Unable to complete the swap process.")
+        except NoSuchElementException:
+            print("Swapping successful")
+
+        globalDriver.switch_to.window(currentWindow)
+
+        globalDriver.refresh()
+
+        editedID = "dgDetails_lblActivity_" + idNumber
+        try:
+            editedRecord = globalDriver.find_element_by_xpath("//span[@id='"+editedID+"' and text()='"+newStatus+" - Pending']")
+        except NoSuchElementException:
+            raise RuntimeError("Unable to find the edited record on the table.")
+
+    def exemptDriver (driverId, exempt, reason):
+        #go_to_driver_in_admin driverId
+
+        if exempt == True:
+            yesRadio = globalDriver.find_element(webElem.DA_EXEMPT_RADIO_YES)
+
+            if yesRadio.is_selected():
+                print("US ELD Exempt is already enabled")
+            else:
+                yesRadio.click()
+                eldComment = globalDriver.find_element(webElem.DA_REASON_TXT)
+                eldComment.click()
+                eldComment.send_keys(reason)
+
+        else:
+            noRadio = globalDriver.find_element(webElem.DA_EXEMPT_RADIO_NO)
+
+            if noRadio.is_selected():
+                print("US ELD Exempt is already disabled")
+            else:
+                noRadio.click()
+
+        action0 = ActionChains(globalDriver)
+        action0.double_click(globalDriver.find_element(webElem.DA_SAVE_BUTTON))
+        action0.perform()
+
+        try:
+            resultElem = globalDriver.find_element_by_xpath("//span[@id='MDriverUC1_messageBar_lblMessage' and text()='"+driverId+" updated.']")
+            print("Successful update")
+        except NoSuchElementException:
+            action0.double_click(globalDriver.find_element(webElem.DA_SAVE_BUTTON))
+            action0.perform()
+            resultElem = globalDriver.find_element_by_xpath("//span[@id='MDriverUC1_messageBar_lblMessage' and text()='" + driverId + " updated.']")
+
+    def setYardMoveTermination(type):
+        globalDriver.get(webElem.ADMIN_URL)
+        globalDriver.find_element(webElem.ADMIN_HOSSETUP_LINK).click()
+
+        yardMoveChck = globalDriver.find_element(webElem.HS_YARDMOVE_CHECKBOX)
+
+        if yardMoveChck.get_property("checked") == "checked":
+            if type == "Power":
+                globalDriver.find_element(webElem.HS_POWER_CHECKBOX)
+            elif type == "Speed":
+                globalDriver.find_element(webElem.HS_SPEED_CHECKBOX)
+            elif type == "Both":
+                globalDriver.find_element(webElem.HS_BOTH_CHECKBOX)
+            else:
+               raise RuntimeError("The type option provided is not a valid value")
+
+        globalDriver.find_element(webElem.HS_SAVE_BUTTON).click()
+
+        try:
+            popup = globalDriver.switch_to.alert()
+            popup.accept()
+        except:
+            print("JS popup did not appear")
+
+        else:
+            raise RuntimeError("Yard move checkbox is not checked")
+
+    def enableDriverYardMove(driverId,enable):
+        globalDriver.get(webElem.ADMIN_DRIVER_URL)
+
+        searchDepot = globalDriver.find_element(webElem.DA_SEARCH_DEPOT_TXT)
+        searchDepot.click()
+        searchDepot.clear()
+        searchDepot.send_keys("All drivers")
+        globalDriver.find_element(webElem.DA_SEARCH_DEPOT_ALLDRI_OPT)
+
+        searchDriver = globalDriver.find_element(webElem.DA_SEARCH_DRIVER_TXT)
+        searchDriver.clear()
+        searchDriver.send_keys(driverId)
+
+        try:
+            driverOpt = globalDriver.find_element_by_xpath("//*[contains(text(),'"+driverId+"')]")
+        except NoSuchElementException:
+            raise RuntimeError("Unable to find the drivers id using the search combo")
+
+        driverOpt.click()
 
 
 
