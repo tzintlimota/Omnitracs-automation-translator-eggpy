cd08aa6301f5b54a7b7a1eca46da4b323e8de3d4
Tzintli Mota <tmaldonado@omnitracs.com>
2021-03-23 16:18:23 -0600

    Update HOS_Status_Test_Case.py
    Complete clock_in method
    Changed default value for last 2 parameters

--- HOS_Status_Test_Case.py
+++ HOS_Status_Test_Case.py
@@ -13,7 +13,7 @@ class HOS_Status_Test_Case(object):
         self.img_proc = self.general.img_proc
         self.ivg_common = IVG_Common(general)
 
-    def clock_in(self, num_min=None, remark1=None, remark2=None, click_continue=None, finish=None):
+    def clock_in(self, num_min=None, remark1=None, remark2=None, reason=None, click_continue=True, finish=True):
         format = '%H:%M'
         format2 = '%M'
         clock_page = self.img_proc.expect_image('vnc-clock-in-screen', 'ExpectedScreens', 2)
@@ -22,28 +22,27 @@ class HOS_Status_Test_Case(object):
             self.img_proc.click_image_by_max_key_points('ELD_Core/StatusTab/ClockInEnabled/ClockInEnabled')
             self.img_proc.expect_image('vnc-clock-in-screen', 'ExpectedScreens', 5)
 
-        print('Currently in CLOCK IN page')
+        print('------Currently in CLOCK IN page------')
 
         current_time2 = self.general.retrieve_text_with_config(285, 315, 170, 233, '--psm 1 --oem 3 -c tessedit_char_whitelist=0123456789:')
 
         # Used regex to remove blanks and jump lines (.strip did not work)
         current_time2 = re.findall(r'\d\d:\d\d', current_time2)
-        print(f'Clock In - Current Time  ({current_time2})')
+        print(f'>>>> Clock In - Current Time  ({current_time2})')
 
         # This is to subtract the minutes (time) to (current_time2) to new_time that will be entered.
         new_time = datetime.strptime(str(current_time2[0]), format) - datetime.strptime(str(num_min), format2)
-        print(f'Clock In - Time to be entered  ({new_time})')
+        print(f'>>>> Clock In - Time to be entered  ({new_time})')
 
         # Split time to have a list with hh and mm separately
         split_time = str(new_time).split(':')
-        print(split_time)
 
         # Click on hours section to enter new_time hh
         self.img_proc.click_image_by_max_key_points_offset(
             "ELD_Core/StatusTab/ClockRemark1/ClockOutRemarks1",
             140, -120)
         #time.sleep(2)
-        print(f'Entering ({split_time[0]}) for HH')
+        print(f'>>>> Entering ({split_time[0]}) for HH')
         self.img_proc.send_keys(split_time[0])
 
         # Click on minutes section to enter new_time mm
@@ -51,9 +50,8 @@ class HOS_Status_Test_Case(object):
             "ELD_Core/StatusTab/ClockRemark1/ClockOutRemarks1",
             170, -120)
 
-
         #time.sleep(2)
-        print(f'Entering ({split_time[1]}) for MM')
+        print(f'>>>> Entering ({split_time[1]}) for MM')
         self.img_proc.send_keys(split_time[1])
 
         # Hide keyboard
@@ -81,11 +79,31 @@ class HOS_Status_Test_Case(object):
             self.img_proc.wait_for_seconds(1)
             self.img_proc.send_keys(remark2)
 
-        # Click on the OK  button
-
-        #self.img_proc.click_image_by_max_key_points('')
-
         # Hide keyboard
         self.img_proc.click_image_by_max_key_points('IVG_Common/Home/KeyboardOpen/KeyboardOpen')
+
+        if click_continue:
+            # Click SAVE button
+            self.img_proc.click_image_by_max_key_points('IVG_Common/Login/OKButton/OKButton')
+            print('>>>> Click in OK button to continue with Clock In')
+
+            self.img_proc.expect_image('vnc-clock-in-enter-reason-screen', 'ExpectedScreens', 3)
+            self.img_proc.click_image_by_max_key_points_offset('ELD_Core/StatusTab/ClockInReason/ReasonForClockIn',
+                                                               0, 40)
+            # Clock IN reason is entered
+            self.img_proc.send_keys(reason)
+
+            # Hide keyboard
+            self.img_proc.click_image_by_max_key_points('IVG_Common/Home/KeyboardOpen/KeyboardOpen')
+            self.img_proc.expect_image('vnc-clock-in-enter-reason-screen', 'ExpectedScreens', 3)
+
+            if finish:
+                self.img_proc.click_image_by_max_key_points('ELD_Core/CertifyTab/SaveButton/SaveButton')
+                print(f'>>>> A CLOCK IN has been set with the time {new_time}')
+            else:
+                print('------Clock In will not be SAVED, currently in REASON FOR CLOCK IN screen...------')
+        else:
+            print('------Clock In will not be confirmed, currently in CLOCK IN screen...------')
+
 
 
