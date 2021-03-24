552166aa8e25b7d53750c49192c5c0c9cc7381a6
Tzintli Mota <tmaldonado@omnitracs.com>
2021-03-24 17:15:16 -0600

    Update OHOS2791.py

--- scripts_translated_py/OHOS2791.py
+++ scripts_translated_py/OHOS2791.py
@@ -10,6 +10,7 @@ from IVG_Common import IVG_Common
 from Daylog_Test_Case import Daylog_Test_Case
 from HOS_Unassigned_Driving_Test_Case import HOS_Unassigned_Driving_Test_Case
 from Certify_Test_Case import Certify_Test_Case
+from HOS_PC_YM import HOS_PC_YM
 import connection_credentials as cfg
 #import pyGPSFeed_IMR
 from ImageProcessor import ImageProcessor
@@ -21,21 +22,33 @@ ivg_common = IVG_Common(gral_access)
 certify = Certify_Test_Case(gral_access)
 daylog = Daylog_Test_Case(gral_access)
 uva_events = HOS_Unassigned_Driving_Test_Case(gral_access)
+pc_ym =HOS_PC_YM(gral_access)
 
 print('log "***Script name OHOS2791***"')
 driver_id = 'JOSH0015'
 
+eld_core.changeDriverStatus('ON', 'YM', '1234', ' ', ' ')
+
 '''ivg_common.logOutAllDrivers()
 ivg_common.loginDriver(driver_id, driver_id, 'True', '')
-eld_core.update_logs()'''
+eld_core.update_logs()
 
 eld_core.goToELD()
 eld_core.changeDriverStatus('OFF', 'PC', '1234', ' ', ' ')
 
 # Run PowerCycle xml vsim file
-gral_access.run_vehsim_script('192.168.100.16', 'C:\ELD_VSIM\PowerCycle.xml', '1')
-
+gral_access.run_vehsim_script('192.168.100.16', 'C:\ELD_VSIM\powercycle1.xml', 1)
+pc_ym.handle_pc_confirm_prompt()
 gral_access.stop_vehsim_script('192.168.100.16')
 
-eld_core.validate_status('Other:PersonalConveyance') #Translator Removed
+eld_core.validate_status('Personal Conveyance') #Translator Removed
 eld_core.dayForward('DayLog', 8)
+
+driver_records = daylog.day_log_records_driver('Bottom', 'Asc', 2)
+print(">>>> Driver Records Retrieved: \n" + str(driver_records))
+
+assert 'PC' in str(driver_records[0][1])
+
+assert 'OFF' in str(driver_records[1][1])'''
+
+
