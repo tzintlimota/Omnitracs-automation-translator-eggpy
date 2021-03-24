54b013c31857acc84b21c3ddb6293861c060eeac
Tzintli Mota <tmaldonado@omnitracs.com>
2021-03-24 17:38:36 -0600

    New get_imui_view_text()

--- HOS_PC_YM.py
+++ HOS_PC_YM.py
@@ -28,10 +28,18 @@ class HOS_PC_YM(object):
         self.ivg_common = IVG_Common(general)
 
     def handle_pc_confirm_prompt(self, confirm=True):
+        print("*** HOS_PC_YM.handle_pc_confirm_prompt ***")
         found = self.img_proc.expect_image('vnc-pc-confirm', 'ExpectedScreens', 5)
         if found and confirm:
             self.img_proc.click_image_by_max_key_points('msg-confirm-yes')
         else:
             self.img_proc.click_image_by_max_key_points('NoButton')
+
+    def get_imui_view_text(self):
+        print("*** HOS_PC_YM.get_imui_view_text ***")
+        y, y1, x, x1 = 200, 400, 10, 1000
+        dot_text = self.general.retrieve_text_with_config(y, y1, x, x1)
+        print(f'Text retrieved from IMUI screen: {str(dot_text).strip()}')
+        return str(dot_text).strip()
 
 
