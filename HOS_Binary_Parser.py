a291f43d6680f84a596b6f0c222784de0775225d
Tzintli Mota <tmaldonado@omnitracs.com>
2021-03-31 14:31:31 -0600

    Update in constructor for class HOS_Binary_Parser

--- HOS_Binary_Parser.py
+++ HOS_Binary_Parser.py
@@ -5,55 +5,60 @@ from selenium.webdriver.support import expected_conditions as EC
 from HOS_Elements import webElem
 import time
 
-globalDriver = webdriver.Chrome()
-globalDriver.implicitly_wait(10)
-
 class HOS_Binary_Parser:
 
+    def __init__(self):
+        self.globalDriver = webdriver.Chrome()
+        self.globalDriver.implicitly_wait(10)
+
     def go_to_parser(self):
-        globalDriver.get(webElem.HOS_WEB_TOOLS_URL)
-        WebDriverWait(globalDriver, 10).until(EC.title_contains(webElem.HOS_WEB_TOOLS_TITLE))
-        globalDriver.maximize_window()
+        print('*** HOS_Binary_Parser.go_to_parser ****')
+        self.globalDriver.get(webElem.HOS_WEB_TOOLS_URL)
+        WebDriverWait(self.globalDriver, 10).until(EC.title_contains(webElem.HOS_WEB_TOOLS_TITLE))
+        self.globalDriver.maximize_window()
 
     def parser_return_message(self, binary_payload):
+        print('*** HOS_Binary_Parser.parser_return_message ****')
         try:
             self.go_to_parser()
 
             #  Click on Return Message Parser Link
-            globalDriver.find_element(*webElem.HOS_WEB_TOOLS_RTN_MSG_PARSER_LINK).click()
+            self.globalDriver.find_element(*webElem.HOS_WEB_TOOLS_RTN_MSG_PARSER_LINK).click()
 
             # Waiting for page to load
-            WebDriverWait(globalDriver, 20).until(
+            WebDriverWait(self.globalDriver, 20).until(
                 EC.presence_of_element_located((By.ID, 'binaryPayload'))
             )
 
             # Click on RETURN MESSAGE PARSER link
             time.sleep(3)
-            globalDriver.find_element(*webElem.HOS_WEB_TOOLS_RTN_MSG_PARSER_INPUT_BOX_ID).click()
-            globalDriver.find_element(*webElem.HOS_WEB_TOOLS_RTN_MSG_PARSER_INPUT_BOX_ID).send_keys(binary_payload)
-            globalDriver.find_element(*webElem.HOS_WEB_TOOLS_RTN_MSG_PARSER_PARSE_BTN).click()
+            self.globalDriver.find_element(*webElem.HOS_WEB_TOOLS_RTN_MSG_PARSER_INPUT_BOX_ID).click()
+            self.globalDriver.find_element(*webElem.HOS_WEB_TOOLS_RTN_MSG_PARSER_INPUT_BOX_ID).send_keys(binary_payload)
+            self.globalDriver.find_element(*webElem.HOS_WEB_TOOLS_RTN_MSG_PARSER_PARSE_BTN).click()
 
             # Wait for value to be parsed and retrieve the text
-            WebDriverWait(globalDriver, 20).until(
+            WebDriverWait(self.globalDriver, 20).until(
                 EC.presence_of_element_located(webElem.HOS_WEB_TOOLS_RTN_MSG_PARSER_TEXT_AREA))
-            text = globalDriver.find_element(*webElem.HOS_WEB_TOOLS_RTN_MSG_PARSER_TEXT_AREA).text
+            text = self.globalDriver.find_element(*webElem.HOS_WEB_TOOLS_RTN_MSG_PARSER_TEXT_AREA).text
 
             print(f'>>>> PARSED Binary Return Message: \n {text}')
             return text
         finally:
-            globalDriver.quit()
+            self.globalDriver.quit()
 
     def get_key_value(self, parsed_message, key_to_search):
-
+        print('*** HOS_Binary_Parser.get_key_value ****')
         split_parsed_msg = parsed_message.split('\n')
 
         for index in range(len(split_parsed_msg)):
-            if key_to_search in split_parsed_msg[index]:
+            if str(key_to_search).lower() in split_parsed_msg[index]:
                 print(f'>>>> Key: "{key_to_search}", has been found')
                 value = split_parsed_msg[index].split('=')
                 print(f'>>>> Key: "{key_to_search}", Value: "{str(value[1]).strip()}"')
                 return str(value[1]).strip()
                 break
+
+
 
 
 
