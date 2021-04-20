5139c6f453d3aa392de0af48eb3f075899d163b5
Tzintli Mota <47425148+tzintlimota@users.noreply.github.com>
2021-04-20 14:08:17 -0500

    Update dependencies_win10.bat

--- dependencies_win10.bat
+++ dependencies_win10.bat
@@ -1,5 +1,9 @@
-pip install -r requirements.txt
 ECHO The dependencies are being installed...
+pip install -r requirements.txt
+pip install opencv-contrib-python
+pip install matplotlib
+pip install vncdotool
+pip install pytesseract
 mkdir	Images
 mkdir 	Images\Buttons
-mkdir	Images\ExpectedSreens
+mkdir	Images\ExpectedScreens
